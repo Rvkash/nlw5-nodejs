7dfafe54732dd0a42b9114a25fec4fd8294a81af
Rvkash <rafael.ieel@hotmail.com>
2021-04-23 19:36:25 -0300

    Next Level Week#5: Aula 3

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response} from "express"
+import { SettingsService } from "../services/SettingsService";
+
+class SettingsController {
+  async create(request: Request, response: Response) {
+  try { 
+    const { chat, username } = request.body;
+    const settingsService = new SettingsService()
+
+    const settings = await settingsService.create({ chat, username })
+
+    return response.json(settings)
+
+  }catch(err) {
+    return response.status(400).json({
+      message: err.message
+    })
+   }
+  }
+}
+
+export { SettingsController }
